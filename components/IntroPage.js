217a419f3923135d17ed78a0c362b2a6af4408df
guotai <guotaixiao87@gmail.com>
2025-05-20 10:09:19 +1000

    optimize layout of introduction page

--- components/IntroPage.js
+++ components/IntroPage.js
@@ -3,7 +3,8 @@ import classes from "./IntroPage.module.css";
 export default function IntroPage() {
     return (
         <div className={classes.main}>
-            
+            <p className={classes.top}>你好</p>
+            <p className={classes.bottom}>Hello, I'm Guotai</p>
         </div>
     );
 }
