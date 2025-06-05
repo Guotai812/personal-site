47eb223718664baa2162ac765a20632853959142
guotai <guotaixiao87@gmail.com>
2025-06-05 15:39:22 +1000

    refactor: change the icon

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,7 @@ import NavBar from "@/components/head/NavBar";
 export const metadata = {
   title: "Guotai",
   description: "Guotai's Site",
+    
 };
 
 export default function RootLayout({ children }) {
